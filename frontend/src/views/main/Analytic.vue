73a9f40355aea482960e433b25bfad06c7fa9e92
polisika <karasyuk@promsoft.ru>
2022-12-24 19:16:58 +0700

    Страницу аналитики наполнил

--- frontend/src/views/main/Analytic.vue
+++ frontend/src/views/main/Analytic.vue
@@ -1,9 +1,22 @@
 <template>
   <v-container fluid>
+    <v-card class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline primary--text">Aggregation</div>
+      </v-card-title>
+      <v-card-text>
+        <div class="headline font-weight-light ma-5">
+          Average error num: {{ agg_err }}
+        </div>
+        <div class="headline font-weight-light ma-5">
+          Average time {{ agg_time }} seconds
+        </div>
+      </v-card-text>
+    </v-card>
     <div v-for="(item, idx) in result" :key="item.id">
       <v-card class="ma-3 pa-3">
         <v-card-title primary-title>
-          <div class="headline primary--text">Attempt №{{ idx + 1 }} describe</div>
+          <div class="headline font-weight">Attempt №{{ idx + 1 }} describe</div>
         </v-card-title>
         <v-card-text>
           <div class="headline font-weight-light ma-5">
@@ -29,11 +42,18 @@ import { ITestResponse } from "@/interfaces";
 @Component
 export default class Table extends Vue {
   result: Array<ITestResponse> = [];
+  agg_err = 0;
+  agg_time = 0;
 
   async mounted() {
     const attempt_id = Number(this.$router.currentRoute.params.attempt);
-    console.log(attempt_id);
     this.result = await api.getAnalytic(this.$store.state.main.token, attempt_id);
+    for (let i = 0; i < this.result.length; i++) {
+      this.agg_err += this.result[i].errors.split(" ").length;
+      this.agg_time += this.result[i].time;
+    }
+    this.agg_err /= this.result.length;
+    this.agg_time /= this.result.length;
   }
 }
 </script>
